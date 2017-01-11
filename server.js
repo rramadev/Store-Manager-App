2580a5d9bc279224d357a9fb4ad7cc7a67c4b316
Raúl <Raul@samsung-pc.com>
2017-01-11 18:46:06 +0100

    Added express-server

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var app = express();
 app.set('port', (process.env.PORT || 8080));
 // app.use(compression());
 app.use(bodyParser.json()); // for parsing application/json
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + ''));
 
 app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(app.get('port'), function() {
